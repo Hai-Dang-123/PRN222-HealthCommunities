efaeffc619151fb16434ed68d1290ef77845f275
Hai-Dang-123 <danglvhse151369@fpt.edu.vn>
2025-02-04 01:31:15 +0700

    push project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HealthCommunitiesCheck2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        
+        //public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
---
+++ Pages/News.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HealthCommunitiesCheck2.Pages
+{
+    public class NewsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
