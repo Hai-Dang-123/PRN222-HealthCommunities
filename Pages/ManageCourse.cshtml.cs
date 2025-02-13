26985fadb44698f49709fa0b9b828241110d389d
Phong <phongthse150256@fpt.edu.vn>
2025-02-13 13:05:57 +0700

    Create page for Course

---
+++ Pages/ManageCourse.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HealthCommunitiesCheck2.Pages
+{
+    public class ManageCourseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
