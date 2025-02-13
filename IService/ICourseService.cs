608656883da93808275a3b1cde020a1860da19b1
Hai-Dang-123 <danglvhse151369@fpt.edu.vn>
2025-02-13 10:30:59 +0700

    check loi

--- IService/ICourseService.cs
+++ IService/ICourseService.cs
@@ -5,5 +5,7 @@ namespace HealthCommunitiesCheck2.IService
     public interface ICourseService
     {
         Task<ResponseDTO> DeleteCourse(Guid courseId);
+        Task<ResponseDTO> Create(Guid courseId);
+
     }
 }
